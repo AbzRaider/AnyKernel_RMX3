d6d2bd95e86839c31e6c93373aeac168170b65ba
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-02-24 21:30:03 +0530

    anykernel : Update Maintainer Info

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Anubis Kernel by Ak
+kernel.string=Azrael Kernel by AbzRaider
 do.devicecheck=1
 do.modules=0
 do.systemless=1
